edf52495c9249bba8d9ccd331420cbb3d00fa735
maknyusblogger <jegerdotkom@gmail.com>
2017-07-06 03:43:39 +0700

    Create popcash.js

---
+++ popcash.js
@@ -0,0 +1,2 @@
+dompc=document.domain;if(dompc=='mp3-musik.wap-ka.com'){var uid = '40914';var wid = '319723'; document.write('<script type="text/javascript" src="http://cdn.popcash.net/pop.js"></script>');}
+ else if(dompc=='retromp3.cf'){var uid = '40914';var wid = '331003'; document.write('<script type="text/javascript" src="http://cdn.popcash.net/pop.js"></script>');} 
